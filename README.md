f8d24bcd5e9517014769c4c6e105dee7c12c6443
maurelio <marcos.aurelio@3esolucoes.com.br>
2025-07-05 16:19:46 -0300

    [FTR] - Finalizando o projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+## Arquivos necessários
+- Microdados Enem 2023
+    - [Link para download](https://download.inep.gov.br/microdados/microdados_enem_2023.zip)
+    - Descompactar os arquivos e mover o arquivo MICRODADOS_ENEM_2023.csv para pasta data
+
+- Variaveis de ambiente
+    - Copiar o arquivo .env.exemplo e nomear de .env
+    - Preencha com os dados de conexão do banco de dados
+
+## Instalação do projeto
+- Rodar os comandos para instalação das dependencias
+    - `python3 -m venv .venv`
+    - `.venv/bin/python3 -m pip install requirements.txt`
---
+++ main.py
@@ -0,0 +1,15 @@
+# TODO: Verificar quais informações eu vou precisar
+# TODO: Estudar o csv e o dicionario
+# TODO: Estruturar o banco de dados
+# TODO: Criar receber os arquivos
+# TODO: Processar os dados
+# TODO: Salvar os dados no banco de dados
+# TODO: Responder perguntas
+
+# 1 - Qual o percentual de faltas dos cantidatos na disciplina de Matemática e Redação?
+# 2 - Se formos olhar para a distribuição das idades dos alunos, como estaria a distribuição dos alunos por faixas etárias?
+# 3 - Quem tem as melhores notas em matemática e redação, candidatos do sexo masculino ou feminino?
+# 4 - Há alguma influência no tipo da escola (publica e privada) nas notas de matemática e redação?
+# 5 - Qual a influência da escolaridade do Pai nas notas de Matemática e Redação?
+# 6 - Qual a influência da escolaridade do Mãe nas notas de Matemática e Redação?
+# 7 - Qual a influência da renda do Pai e da Mãe nas notas de Matemática e Redação?
